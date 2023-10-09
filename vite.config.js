092392c30a6a2feeb590ace17b4039dc49397954
suzi <suziting2016@gmail.com>
2023-10-09 18:36:51 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  history: createWebHistory('/learn/'),
   plugins: [vue()],
   base: '/learn/'
 })
