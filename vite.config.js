033b98ec201e1c26cac6f5e6de1a7a73e0335fb7
suzi <suziting2016@gmail.com>
2023-10-09 19:08:49 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import {createWebHistory} from "vue-router"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  history: createWebHistory('/vite/'),
+  // history: createWebHistory('/vite/'),
   plugins: [vue()],
-  base: '/vite/'
+  base: '/vite/',
+  build: {
+    outDir: 'docs' // 打包文件的输出目录
+  }
 })
